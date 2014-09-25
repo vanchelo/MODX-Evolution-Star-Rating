b34d9e9cbf22a6c951e16764ffe7eb3ba5a1f1f6
Ivan Brezhnev <npobolka@ya.ru>
2014-09-25 15:06:04 +0300

    Update star_rating.sql

--- assets/snippets/star_rating/star_rating.sql
+++ assets/snippets/star_rating/star_rating.sql
@@ -1,25 +1,22 @@
--- Adminer 3.6.3 MySQL dump
-
 SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `modx_star_rating`;
 CREATE TABLE `modx_star_rating` (
   `rid` int(11) unsigned NOT NULL COMMENT 'Resource ID',
-  `total` int(5) unsigned default NULL COMMENT 'Rating',
-  `votes` int(5) unsigned default NULL COMMENT 'Total Votes',
-  PRIMARY KEY  (`rid`)
+  `total` int(5) unsigned DEFAULT NULL COMMENT 'Rating',
+  `votes` int(5) unsigned DEFAULT NULL COMMENT 'Total Votes',
+  PRIMARY KEY (`rid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `modx_star_rating_votes`;
 CREATE TABLE `modx_star_rating_votes` (
-  `id` int(11) unsigned NOT NULL auto_increment,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `rid` int(11) unsigned NOT NULL COMMENT 'Resource ID',
-  `vote` tinyint(1) unsigned NOT NULL default '0' COMMENT 'Vote',
-  `ip` varchar(15) NOT NULL default '0.0.0.0' COMMENT 'IP Address',
-  `time` int(11) unsigned NOT NULL default '0' COMMENT 'Voted Time',
-  PRIMARY KEY  (`id`)
+  `vote` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Vote',
+  `ip` varchar(15) NOT NULL DEFAULT '0.0.0.0' COMMENT 'IP Address',
+  `time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Voted Time',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
--- 2013-04-19 12:31:32
