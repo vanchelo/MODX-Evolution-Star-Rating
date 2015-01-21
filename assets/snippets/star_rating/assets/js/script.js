3ff620ece3cf5fbd2efce3ad4a435739d5b6e647
vanchelo <npobolka@ya.ru>
2015-01-21 22:20:17 +0200

    Обновлен скрипт для множественных вызовов

--- assets/snippets/star_rating/assets/js/script.js
+++ assets/snippets/star_rating/assets/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    var rating = $('.star-rating-container');
-
-    rating.on('click', 'a', function (e) {
+    $('.star-rating-container').on('click', 'a', function (e) {
         e.preventDefault();
 
-        var vote = $(this).data('vote'),
-            id = $(this).parents('ul').data('pid');
+        var link = $(this),
+            container = link.closest('.star-rating-container'),
+            vote = link.data('vote'),
+            id = link.parents('ul').data('pid');
 
         $.ajax({
             url: 'assets/snippets/star_rating/connector.php',
@@ -16,23 +16,23 @@ $(document).ready(function () {
             },
             success: function (data) {
                 if (data) {
-                    rating.find('.msg').remove();
+                    container.find('.msg').remove();
                     if (data.success === true && !data.error) {
-                        rating.html(data.html);
+                        container.html(data.html);
                     }
-                    rating.append('<div class="msg">' + data.message + '</div>');
-                    rating.find('.mask').fadeOut(100, function () {
+                    container.append('<div class="msg">' + data.message + '</div>');
+                    container.find('.mask').fadeOut(100, function () {
                         $(this).remove();
                     });
                     setTimeout(function () {
-                        rating.find('.msg').fadeOut(1000)
+                        container.find('.msg').fadeOut(1000)
                     }, 1500)
                 } else {
                     alert('Неизвестная ошибка, повторите попытку еще раз');
                 }
             },
             beforeSend: function () {
-                rating.append('<div class="mask" />');
+                container.append('<div class="mask" />');
             }
         })
     });
