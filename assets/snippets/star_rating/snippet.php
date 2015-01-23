5931607caea09b4892defef1af0a1680da989984
vanchelo <npobolka@ya.ru>
2015-01-23 16:34:46 +0200

    Краткое описание сниппета

--- assets/snippets/star_rating/snippet.php
+++ assets/snippets/star_rating/snippet.php
@@ -1,24 +1,21 @@
 <?php
 /** @var DocumentParser $modx */
-/*
+
+/**
  * Сниппет - Звездный рейтинг
  *
- * Вызов на странице - [!star_rating!]
- * Добавить в настройках системы тип новый тип содержимого - application/json
- * Создать новый документ с пустым шаблоном
- * Выбрать тип содержимого - application/json
- * Вставить в содержимое созданного ресурса - [!star_rating!]
- * Ввести в поле алиаc - star-rating
+ * Пример вызова на странице:
+ * [!star_rating? $id=`[*id*]` &tpl=`@CHUNK:star_rating`!]
  */
 
 require_once MODX_BASE_PATH . 'assets/snippets/star_rating/starrating.class.php';
 
 $config = array(
+    'id' => isset($id) ? (int) $id : $modx->documentObject['id'],
     'tpl' => isset($tpl) ? (string) $tpl : 'template',
     'lang' => isset($lang) ? (string) $lang : 'ru',
     'interval' => isset($interval) ? (int) $interval : 24 * 60 * 60,
     'width' => isset($width) ? (int) $width : 16,
-    'id' => isset($id) ? (int) $id : $modx->documentObject['id'],
     'noJs' => isset($noJs) ? (bool) $noJs : false,
     'noCss' => isset($noCss) ? (bool) $noCss : false,
 );
